68ee892448b894af02e4f5dc6d9b04622d963186
Kushal Shah <kushalshah821@gmail.com>
2023-01-26 11:31:41 +0530

    changed path to images folder

--- src/main/java/com/univ/backend/constants/Constant.java
+++ src/main/java/com/univ/backend/constants/Constant.java
@@ -2,7 +2,7 @@ package com.univ.backend.constants;
 
 public class Constant {
     public static final String BASE_URL = "";
-    public static final String IMAGE_BASE_URL = "backend/images/";
+    public static final String IMAGE_BASE_URL = "images/";
     public static final String TEAM_SAVE_URL = IMAGE_BASE_URL + "team/";
     public static final String SPONSOR_SAVE_URL = IMAGE_BASE_URL + "sponsor/";
 }
