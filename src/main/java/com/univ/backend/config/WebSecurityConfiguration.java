6b0b03b30d588a083f9ed8f8442409da3b2a5b25
Kushal Shah <kushalshah821@gmail.com>
2023-01-26 11:41:27 +0530

    cors eror 123

--- src/main/java/com/univ/backend/config/WebSecurityConfiguration.java
+++ src/main/java/com/univ/backend/config/WebSecurityConfiguration.java
@@ -17,8 +17,11 @@ public class WebSecurityConfiguration  {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*").allowedOrigins("https://localhost:5173");
-                WebMvcConfigurer.super.addCorsMappings(registry);
+                registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "DELETE", "PUT")
+                        .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                                "Access-Control-Request-Headers")
+                        .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                        .allowCredentials(true).maxAge(3600);
             }
         };
     }
