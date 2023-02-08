8fe93d499387ce36bf451f7a2fe4274b08a13a7a
= <=>
2023-02-08 12:54:53 +0530

    bug fixes

--- src/main/java/com/univ/backend/services/ExpertiseService.java
+++ src/main/java/com/univ/backend/services/ExpertiseService.java
@@ -3,6 +3,8 @@ package com.univ.backend.services;
 import com.univ.backend.entities.Expertise;
 import com.univ.backend.exceptions.ExpertiseNotFoundException;
 import com.univ.backend.exceptions.ImageFormatException;
+import com.univ.backend.exceptions.MandatoryFieldFoundEmptyException;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileNotFoundException;
@@ -14,7 +16,7 @@ public interface ExpertiseService {
 
     Expertise getExpertiseByName(String name);
 
-    Expertise postExpertiseWithDetails(Expertise expertise, MultipartFile bg);
+    Expertise postExpertiseWithDetails(Expertise expertise, MultipartFile bg) throws IOException, ImageFormatException, MandatoryFieldFoundEmptyException;
 
     Expertise updateExpertiseUsingId(Expertise expertise, MultipartFile bg, Long id) throws ExpertiseNotFoundException, IOException, ImageFormatException;
 
